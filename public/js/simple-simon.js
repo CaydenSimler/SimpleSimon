2d4ad2f01b661cdee9e1238fef208702a519e0e2
Cayden <cayden.simler@hotmail.com>
2017-03-02 14:43:08 -0600

    Added the initial files to the repository including bootstrap files for css and js, as well as the initial setup files for the simple-simon game

---
+++ public/js/simple-simon.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+
+$(".greenbox").click(function(){
+	$(".greenbox").css("background-color", "#1C7715");	
+
+	setTimeout(function(){
+		$(".greenbox").css("background-color", "#5F9E5B");
+	}, 500);
+});
+
+$(".redbox").click(function(){
+	$(".redbox").css("background-color", "#A0291E");	
+	
+	setTimeout(function(){
+		$(".redbox").css("background-color", "#C16C64");
+	}, 500);
+});
+
+$(".yellowbox").click(function(){
+	$(".yellowbox").css("background-color", "#D8BC1A");	
+	
+	setTimeout(function(){
+		$(".yellowbox").css("background-color", "#D8C970");
+	}, 500);
+});
+
+$(".bluebox").click(function(){
+	$(".bluebox").css("background-color", "#174DC1");	
+	
+	setTimeout(function(){
+		$(".bluebox").css("background-color", "#6884C1");
+	}, 500);
+});

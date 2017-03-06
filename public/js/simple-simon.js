4490456d8b399921cbd3f41e8efac73bb3674ff4
Cayden <cayden.simler@hotmail.com>
2017-03-06 09:33:06 -0600

    Removed the function for converting the arrays into strings, that way that can be done after a specific round function is fired. Also removed the functionality for firing a color change after the random number generator function is fired, because if it's only fired once, it will fire all the colors at once rather than in a sequence.

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -11,34 +11,10 @@ function randomNumGen(){
 	
 	colorOrder.push(randomNumber);
 	console.log(colorOrder);
-	
-	if (randomNumber == 1) {
-		$(".redbox").css("background-color", "#A0291E");	
-		
-		setTimeout(function(){
-			$(".redbox").css("background-color", "#C16C64");
-		}, 500);	
-	} else if (randomNumber == 2) {
-		$(".yellowbox").css("background-color", "#D8BC1A");	
-		
-		setTimeout(function(){
-			$(".yellowbox").css("background-color", "#D8C970");
-		}, 500);
-	} else if (randomNumber == 3) {
-		$(".bluebox").css("background-color", "#174DC1");	
-		
-		setTimeout(function(){
-			$(".bluebox").css("background-color", "#6884C1");
-		}, 500);	
-	} else {
-		$(".greenbox").css("background-color", "#1C7715");	
-
-		setTimeout(function(){
-			$(".greenbox").css("background-color", "#5F9E5B");
-		}, 500);
-	}
 }
 
+// Converts the string back into an array that can then be modified after each color is added to the sequence
+// This needs to be run after each individual round in order to add more items onto each array
 function convertString(){
 	colorOrder = colorOrder.split(",");
 	inputOrder = inputOrder.split(",");
@@ -47,23 +23,13 @@ function convertString(){
 	console.log(inputOrder);
 }
 
-function runRound(){
-	randomNumGen()
 
-	colorOrder = colorOrder.join();
-	inputOrder = inputOrder.join()
-	console.log(colorOrder);
-	console.log(inputOrder);
+// colorOrder = colorOrder.join();
+// inputOrder = inputOrder.join();
 
-	if (inputOrder == colorOrder) {
-		console.log("success");
-	} else {
-		console.log("fail");
-	}
 
-	convertString();
-}
 
+// Clears the inputOrder and colorOrder that way the game can have a full refresh before it runs
 $(".startButton").click(function(){
 	console.log("test start");
 	colorOrder = [];
@@ -72,14 +38,11 @@ $(".startButton").click(function(){
 	console.log(inputOrder);
 })
 
-// After each round:
-// 	colorOrder has to be split into a string
-// 	convert colorOrder into a string that can be compared using .join()
-// 	colorOrder and inputOrder have to be compared as strings because they don't evaluate to true as an array
 
-// 	after they are compared and evaluate to true, colorOrder has to be converted back into an array using .split(",") 
-// 	that way more values can be added onto the array
-
+// All four of these buttons are for basic functionality of how the buttons work
+// The corresponding number to each button is appended onto the inputOrder array
+// The default color changes to a darker shade to indicate that the button has been pushed
+// After half a second, the button changes back to the lighter shade
 
 $(".redbox").click(function(){
 

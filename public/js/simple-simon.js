bac226e49a37055471cbdee44a5f3bcc9bff7a35
Cayden <cayden.simler@hotmail.com>
2017-03-07 09:29:40 -0600

    Added the checkOrder function to compare the colorOrder and inputOrder to each other

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -6,14 +6,18 @@ var colorOrder = [],
 	inputSequence = 0,
 	index = 0;
 
+//////////////////////////////////////////////////////////////////////////////////
 // Generate a random number between 1 and 4 then adds it onto the colorOrder array
+//////////////////////////////////////////////////////////////////////////////////
 function randomNumGen(){
 	var randomNumber = (Math.floor(Math.random() * 4) + 1).toString();
 	colorOrder.push(randomNumber);
 	console.log(colorOrder);
 }
 
+/////////////////////////////
 // Add 1 to the round counter
+/////////////////////////////
 function addRound(){
 	roundNumber++;
 	index = 0;
@@ -21,8 +25,9 @@ function addRound(){
 }
 
 
-
+///////////////////////////////////////////////////////////////////////
 // Calls the checkColor function for each index in the colorOrder array
+///////////////////////////////////////////////////////////////////////
 function increment(){
 	setTimeout(function(){
 		if (index < colorOrder.length) {
@@ -32,27 +37,54 @@ function increment(){
 	}, 800);
 }
 
-// Runs the next round by adding on a 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Runs the next round by adding on a new color to colorOrder, adds a round to the counter, and runs the increment
+// command in order to display each color in order of index
+///////////////////////////////////////////////////////////
 function round() {
 	randomNumGen()
 	addRound();
 	increment();
 }
 
+///////////////////////////////////////////////////////////////////
+// A function that compares the length of inputOrder and colorOrder
+///////////////////////////////////////////////////////////////////
 function checkLength() {
 	if (inputOrder.length == colorOrder.length) {
-		console.log("fired length checker");
+		///////////////////////////////////////////////////////////////////////////////////
+		// Converts both the inputOrder and colorOrder into strings so they can be compared
+		///////////////////////////////////////////////////////////////////////////////////
 		toString();
-		console.log(colorOrder);
-		console.log(inputOrder);
+		checkOrder();
 	} else {
 		inputSequence++
-		console.log("sequence position: " + inputSequence);
-		console.log("color order length: " + colorOrder.length);
 	}
 }
 
+////////////////////////////////////////////////////////////////////////
+// A function that compares the length of both inputOrder and colorOrder
+////////////////////////////////////////////////////////////////////////
+function checkOrder(){
+	if (colorOrder == inputOrder){
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+		// Converts the colorOrder and inputOrder back into arrays so more colors can be added to the sequence
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+		toArray();
+
+		inputSequence = 0;
+		inputOrder = [];
+		round();
+
+	} else {
+		alert("colorOrder != inputOrder");
+	}
+	
+}
+
+///////////////////////////////////////////////////////////////////////
 // Checks which color is at the specified index in the colorOrder array
+///////////////////////////////////////////////////////////////////////
 function checkColor(color) {
 
 		if (color == 1) {
@@ -86,40 +118,47 @@ function checkColor(color) {
 		increment();
 };
 
-
+//////////////////////////////////////////////////////////////////////////
 // Changes the colorOrder and inputOrder into strings that can be compared
+//////////////////////////////////////////////////////////////////////////
 function toString(){
 	colorOrder = colorOrder.join();
 	inputOrder = inputOrder.join();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Converts the string back into an array that can then be modified after each color is added to the sequence
 // This needs to be run after each individual round in order to add more items onto each array
+//////////////////////////////////////////////////////////////////////////////////////////////
 function toArray(){
 	colorOrder = colorOrder.split(",");
 	inputOrder = inputOrder.split(",");
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Clears the inputOrder, colorOrder, and roundNumber that way the game can have a full refresh before it runs
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $(".start").click(function(){
-	// $(".start").removeClass("startButton").html("");
-	$(".highScoreNumber").html(roundNumber + 1);
-	roundNumber = -1;
-	$(".roundNumber").html(roundNumber - roundNumber);
+	//////////////////////////////////////////////////
+	// Resets both the inputOrder and colorOrder array
+	//////////////////////////////////////////////////
 	colorOrder = [];
 	inputOrder = [];
-
+	//////////////////////////////////////////////////////////
 	// Fires the function to start the game at the first round
+	//////////////////////////////////////////////////////////
 	round();
-})
+});
 
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // All four of these buttons are for basic functionality of how each input as added onto the inputOrder array
 // The corresponding number to each button is appended onto the inputOrder array
 // 1 = Red, 2 = Yellow, etc...
 // The default color changes to a darker shade to indicate that the button has been pushed
 // After half a second, the button changes back to the lighter shade
-
+////////////////////////////////////////////////////////////////////
 $(".redbox").click(function(){
 
 	inputOrder.push("1");
